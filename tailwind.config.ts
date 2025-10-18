4528b9ce15cfcc0c1ecdcca34ffc96d303c07db3
ʀ ᴏ ʜ ɪ ᴛ <rohit75822@gmail.com>
2025-10-18 12:11:18 +0000

    Comic Relief
    use this font style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Space Grotesk', 'sans-serif'],
+        body: ['"Comic Relief"', 'cursive'],
+        headline: ['"Comic Relief"', 'cursive'],
         code: ['monospace'],
       },
       colors: {
